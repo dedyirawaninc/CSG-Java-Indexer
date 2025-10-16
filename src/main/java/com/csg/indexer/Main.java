019d36d25910365cf2be20aa91f44279bd0039fe
Dedy Irawan inc <dedy.irawan.inc@gmail.com>
2025-10-16 19:36:33 +0700

    Initial commit: indexing CLI with rules

--- src/main/java/com/csg/indexer/Main.java
+++ src/main/java/com/csg/indexer/Main.java
@@ -1,9 +1,14 @@
 package com.csg.indexer;
 
-import java.nio.file.Path;
-
 import com.csg.indexer.io.FileLoader;
 import com.csg.indexer.parser.ContentExtractor;
+import com.csg.indexer.rules.IndexRule;
+import com.csg.indexer.rules.LongWordsRule;
+import com.csg.indexer.rules.UppercaseStartCountRule;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,13 +16,24 @@ public class Main {
             System.err.println("Usage: java -jar java-indexer.jar <file1> <file2> ...");
             System.exit(1);
         }
-        
+
+        List<IndexRule> rules = new ArrayList<>();
+        rules.add(new UppercaseStartCountRule());
+        rules.add(new LongWordsRule(5));
+
         for (String fileArg : args) {
             Path path = Path.of(fileArg);
+            
             try {
-                byte[] fileBytes = FileLoader.readBytes(path);
-                String extractedText = ContentExtractor.extractText(fileBytes, path);
-                System.out.println("Extracted text from " + path + ":\n" + extractedText);
+                String content = ContentExtractor.extractText(FileLoader.readBytes(path), path);
+                System.out.println("=== File: " + path.toString() + " ===");
+
+                for (IndexRule rule : rules) {
+                    System.out.println(rule.name() + ":");
+                    System.out.println(rule.apply(content));
+                }
+
+                System.out.println();
             }
             catch (Exception e) {
                 System.err.println("Error processing file " + path + ": " + e.getMessage());
